2d29d911844c70a6c820c7fd5947d46393126a7d
Keran Yang <yangkr920208@gmail.com>
2023-02-03 10:36:04 -0500

    Address comments
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- examples/src/main/java/io/numaproj/numaflow/examples/sink/simple/SimpleSink.java
+++ examples/src/main/java/io/numaproj/numaflow/examples/sink/simple/SimpleSink.java
@@ -8,6 +8,7 @@ import io.numaproj.numaflow.sink.SinkServer;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -23,6 +24,7 @@ public class SimpleSink {
             if (datum == SinkDatumStream.EOF) {
                 break;
             }
+            logger.info(Arrays.toString(datum.getValue()));
             responses.add(new Response(datum.getId(), true, ""));
         }
         return responses;
