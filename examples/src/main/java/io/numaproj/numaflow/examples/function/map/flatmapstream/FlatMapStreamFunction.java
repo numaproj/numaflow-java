fc6b18879be59bcedd2fe8bca7c20706774e2b37
xdevxy <115589853+xdevxy@users.noreply.github.com>
2023-06-07 17:57:24 -0700

    chore: add map stream udf example (#52)
    Signed-off-by: Hao Hao <xdevxhao@gmail.com>

---
+++ examples/src/main/java/io/numaproj/numaflow/examples/function/map/flatmapstream/FlatMapStreamFunction.java
@@ -0,0 +1,32 @@
+package io.numaproj.numaflow.examples.function.map.flatmapstream;
+
+import io.grpc.stub.StreamObserver;
+
+import io.numaproj.numaflow.function.v1.Udfunction;
+import io.numaproj.numaflow.function.FunctionServer;
+import io.numaproj.numaflow.function.handlers.MapStreamHandler;
+import io.numaproj.numaflow.function.interfaces.Datum;
+import io.numaproj.numaflow.function.types.Message;
+
+/**
+ * This is a simple User Defined Function example which processes the input message
+ * and produces more than one output messages(flatMap) in a streaming mode
+ * example : if the input message is "dog,cat", it streams two output messages
+ * "dog" and "cat"
+ */
+
+public class FlatMapStreamFunction extends MapStreamHandler {
+
+    public static void main(String[] args) throws Exception {
+        new FunctionServer().registerMapStreamHandler(new FlatMapStreamFunction()).start();
+    }
+
+    public void processMessage(String[] keys, Datum data, StreamObserver<Udfunction.DatumResponse> streamObserver) {
+        String msg = new String(data.getValue());
+        String[] strs = msg.split(",");
+
+        for (String str : strs) {
+            onNext(new Message(str.getBytes()), streamObserver);
+        }
+    }
+}
