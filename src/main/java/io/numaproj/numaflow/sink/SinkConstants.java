0761e09cd4baddf5fdd4957e84fdfaa4fbe4a71a
Derek Wang <whynowy@gmail.com>
2023-07-07 15:29:50 -0700

    chore: update default gPRC msg size to 64 MB (#60)
    Signed-off-by: Derek Wang <whynowy@gmail.com>

--- src/main/java/io/numaproj/numaflow/sink/SinkConstants.java
+++ src/main/java/io/numaproj/numaflow/sink/SinkConstants.java
@@ -3,7 +3,7 @@ package io.numaproj.numaflow.sink;
 class SinkConstants {
     public static final String DEFAULT_SOCKET_PATH = "/var/run/numaflow/udsink.sock";
 
-    public static final int DEFAULT_MESSAGE_SIZE = 1024 * 1024 * 4;
+    public static final int DEFAULT_MESSAGE_SIZE = 1024 * 1024 * 64;
 
     public static final String SUCCESS = "SUCCESS";
 
