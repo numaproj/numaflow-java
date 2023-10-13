6f830c64af30e10e5930b5002746ce0bb9694dd7
Keran Yang <yangkr920208@gmail.com>
2023-10-13 13:04:48 -0400

    .
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- src/main/java/io/numaproj/numaflow/sourcetransformer/Message.java
+++ src/main/java/io/numaproj/numaflow/sourcetransformer/Message.java
@@ -11,8 +11,11 @@ import java.time.Instant;
 @Getter
 public class Message {
     public static final String DROP = "U+005C__DROP__";
-
-
+    // epoch second -86400 translates to UTC time 1969-12-31 00:00:00 +0000 UTC,
+    // EventTimeForDrop is set to be slightly earlier than Unix epoch -1 (1969-12-31 23:59:59.999 +0000 UTC)
+    // As -1 is used on Numaflow to indicate watermark is not available,
+    // EventTimeForDrop is used to indicate that the message is dropped hence, excluded from watermark calculation
+    public static final Instant EventTimeForDrop = Instant.ofEpochSecond(-86400);
     private final String[] keys;
     private final byte[] value;
     private final Instant eventTime;
@@ -60,6 +63,10 @@ public class Message {
      * @return returns the Message which will be dropped
      */
     public static Message toDrop() {
-        return new Message(new byte[0], Instant.MIN, null, new String[]{DROP});
+        return new Message(
+                new byte[0],
+                EventTimeForDrop,
+                null,
+                new String[]{DROP});
     }
 }
