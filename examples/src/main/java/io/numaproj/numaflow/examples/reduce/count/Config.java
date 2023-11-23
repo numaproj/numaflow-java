a072f6d3c61aec817f903c8de3a9056ec8a3f160
Yashash H L <yashashhl25@gmail.com>
2023-11-23 08:40:34 +0530

    fix: counter example (#85)
    Signed-off-by: Yashash H L <yashashhl25@gmail.com>

--- examples/src/main/java/io/numaproj/numaflow/examples/reduce/count/Config.java
+++ examples/src/main/java/io/numaproj/numaflow/examples/reduce/count/Config.java
@@ -6,6 +6,5 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class Config {
-    private int oddIncrementBy;
-    private int evenIncrementBy;
+    private int incrementBy;
 }
---
+++ examples/src/main/java/io/numaproj/numaflow/examples/reduce/count/CounterFactory.java
@@ -0,0 +1,63 @@
+package io.numaproj.numaflow.examples.reduce.count;
+
+import io.numaproj.numaflow.reducer.Datum;
+import io.numaproj.numaflow.reducer.Message;
+import io.numaproj.numaflow.reducer.MessageList;
+import io.numaproj.numaflow.reducer.Metadata;
+import io.numaproj.numaflow.reducer.Reducer;
+import io.numaproj.numaflow.reducer.ReducerFactory;
+import io.numaproj.numaflow.reducer.Server;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * CounterFactory is used to create Counter Reducer.
+ * Counter Reducer is used to count the number of messages received.
+ * it increments the count based on the value specified in the config.
+ */
+@Slf4j
+@AllArgsConstructor
+public class CounterFactory extends ReducerFactory<CounterFactory.Counter> {
+    private Config config;
+
+    public static void main(String[] args) throws Exception {
+        log.info("counter udf was invoked");
+        Config config = new Config(1);
+        new Server(new CounterFactory(config)).start();
+    }
+
+    @Override
+    public Counter createReducer() {
+        return new Counter(config);
+    }
+
+    @Slf4j
+    public static class Counter extends Reducer {
+        private final Config config;
+        private int count;
+
+        public Counter(Config config) {
+            this.config = config;
+        }
+
+        @Override
+        public void addMessage(String[] keys, Datum datum, Metadata md) {
+                // increment based on the value specified in the config
+                count += config.getIncrementBy();
+        }
+
+        @Override
+        public MessageList getOutput(String[] keys, Metadata md) {
+            log.info(
+                    "count - {}, window - {} {}",
+                    count,
+                    md.getIntervalWindow().getStartTime().toString(),
+                    md.getIntervalWindow().getEndTime().toString());
+
+            return MessageList
+                    .newBuilder()
+                    .addMessage(new Message(String.valueOf(count).getBytes(), keys))
+                    .build();
+        }
+    }
+}
