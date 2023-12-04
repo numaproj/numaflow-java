06b90a0833690f6b5bf5501b0f6a22f413130b53
Keran Yang <yangkr920208@gmail.com>
2023-12-04 18:44:22 -0500

    fix: require event time when dropping messages (#86)
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- examples/src/main/java/io/numaproj/numaflow/examples/sourcetransformer/eventtimefilter/EventTimeFilterFunction.java
+++ examples/src/main/java/io/numaproj/numaflow/examples/sourcetransformer/eventtimefilter/EventTimeFilterFunction.java
@@ -12,8 +12,8 @@ import java.time.Instant;
  * This is a simple User Defined Function example which receives a message, applies the following
  * data transformation, and returns the message.
  * <p>
- * If the message event time is before year 2022, drop the message. If it's within year 2022, update
- * the tag to "within_year_2022" and update the message event time to Jan 1st 2022.
+ * If the message event time is before year 2022, drop the message with the original event time.
+ * If it's within year 2022, update the tag to "within_year_2022" and update the message event time to Jan 1st 2022.
  * Otherwise, (exclusively after year 2022), update the tag to "after_year_2022" and update the
  * message event time to Jan 1st 2023.
  */
@@ -31,7 +31,7 @@ public class EventTimeFilterFunction extends SourceTransformer {
         Instant eventTime = data.getEventTime();
 
         if (eventTime.isBefore(januaryFirst2022)) {
-            return MessageList.newBuilder().addMessage(Message.toDrop()).build();
+            return MessageList.newBuilder().addMessage(Message.toDrop(eventTime)).build();
         } else if (eventTime.isBefore(januaryFirst2023)) {
             return MessageList
                     .newBuilder()
