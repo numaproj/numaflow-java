f5c324e93438bfaddb278c55150ca8115a0a49a3
Yashash H L <yashashhl25@gmail.com>
2024-10-13 17:45:32 +0530

    feat: Unify MapStream and Unary Map Operations Using a Shared gRPC Protocol
    Signed-off-by: Yashash H L <yashashhl25@gmail.com>

--- src/test/java/io/numaproj/numaflow/mapstreamer/ServerTest.java
+++ src/test/java/io/numaproj/numaflow/mapstreamer/ServerTest.java
@@ -5,8 +5,8 @@ import io.grpc.ManagedChannel;
 import io.grpc.inprocess.InProcessChannelBuilder;
 import io.grpc.inprocess.InProcessServerBuilder;
 import io.grpc.testing.GrpcCleanupRule;
-import io.numaproj.numaflow.mapstream.v1.MapStreamGrpc;
-import io.numaproj.numaflow.mapstream.v1.Mapstream;
+import io.numaproj.numaflow.map.v1.MapGrpc;
+import io.numaproj.numaflow.map.v1.MapOuterClass;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -14,8 +14,11 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class ServerTest {
     private final static String PROCESSED_KEY_SUFFIX = "-key-processed";
@@ -58,36 +61,55 @@ public class ServerTest {
 
     @Test
     public void TestMapStreamer() {
-        ByteString inValue = ByteString.copyFromUtf8("invalue");
-        Mapstream.MapStreamRequest inDatum = Mapstream.MapStreamRequest
+        MapOuterClass.MapRequest handshakeRequest = MapOuterClass.MapRequest
                 .newBuilder()
-                .addAllKeys(List.of("test-map-stream-key"))
-                .setValue(inValue)
+                .setHandshake(MapOuterClass.Handshake.newBuilder().setSot(true))
                 .build();
+
+        ByteString inValue = ByteString.copyFromUtf8("invalue");
+        MapOuterClass.MapRequest request = MapOuterClass.MapRequest
+                .newBuilder()
+                .setRequest(MapOuterClass.MapRequest.Request.newBuilder()
+                        .setValue(inValue)
+                        .addKeys("test-map-stream-key")).build();
 
         String[] expectedKeys = new String[]{"test-map-stream-key" + PROCESSED_KEY_SUFFIX};
         String[] expectedTags = new String[]{"test-tag"};
         ByteString expectedValue = ByteString.copyFromUtf8("invalue" + PROCESSED_VALUE_SUFFIX);
 
-        var stub = MapStreamGrpc.newBlockingStub(inProcessChannel);
+        MapStreamOutputStreamObserver mapStreamOutputStreamObserver = new MapStreamOutputStreamObserver(
+                12);
+        var stub = MapGrpc.newStub(inProcessChannel);
 
-        var actualDatumList = stub
-                .mapStreamFn(inDatum);
+        var requestStreamObserver = stub
+                .mapFn(mapStreamOutputStreamObserver);
 
+        requestStreamObserver.onNext(handshakeRequest);
+        requestStreamObserver.onNext(request);
+        requestStreamObserver.onCompleted();
 
-        int count = 0;
-        while (actualDatumList.hasNext()) {
-            Mapstream.MapStreamResponse d = actualDatumList.next();
-            assertEquals(
-                    expectedKeys,
-                    d.getResult().getKeysList().toArray(new String[0]));
-            assertEquals(expectedValue, d.getResult().getValue());
-            assertEquals(
-                    expectedTags,
-                    d.getResult().getTagsList().toArray(new String[0]));
-            count++;
+        try {
+            mapStreamOutputStreamObserver.done.get();
+        } catch (InterruptedException | ExecutionException e) {
+            fail("exception thrown");
         }
-        assertEquals(10, count);
+        System.out.println("got responses");
+        List<MapOuterClass.MapResponse> messages = mapStreamOutputStreamObserver.getMapResponses();
+        // 10 responses + 1 handshake response + 1 eot response
+        assertEquals(12, messages.size());
+
+        // first is handshake
+        assertTrue(messages.get(0).hasHandshake());
+
+        messages = messages.subList(1, messages.size());
+        for (MapOuterClass.MapResponse response : messages) {
+            if (response.hasStatus()) {
+                assertTrue(response.getStatus().getEot());
+                continue;
+            }
+            assertEquals(expectedValue, response.getResults(0).getValue());
+            assertEquals(expectedKeys[0], response.getResults(0).getKeys(0));
+        }
     }
 
     private static class TestMapStreamFn extends MapStreamer {
