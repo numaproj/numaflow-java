57e4214bc47f73b7fb9610e93b57ced87627c490
Keran Yang <yangkr920208@gmail.com>
2025-05-03 13:09:59 -0400

    fix: revert sideinput Constant back to public (#182)
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- src/main/java/io/numaproj/numaflow/sideinput/Constants.java
+++ src/main/java/io/numaproj/numaflow/sideinput/Constants.java
@@ -1,6 +1,6 @@
 package io.numaproj.numaflow.sideinput;
 
-class Constants {
+public class Constants {
   public static final String SIDE_INPUT_DIR = "/var/numaflow/side-inputs";
   static final String DEFAULT_SOCKET_PATH = "/var/run/numaflow/sideinput.sock";
   static final String DEFAULT_SERVER_INFO_FILE_PATH = "/var/run/numaflow/sideinput-server-info";
