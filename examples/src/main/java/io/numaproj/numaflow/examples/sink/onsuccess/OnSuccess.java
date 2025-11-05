224d7752fee31b52b6dca973f260419896cc875f
vtiwari5 <vaibhav_tiwari1@intuit.com>
2025-11-05 15:28:11 -0500

    Modify onSuccess message in the added example for clarity
    Signed-off-by: vtiwari5 <vaibhav_tiwari1@intuit.com>

--- examples/src/main/java/io/numaproj/numaflow/examples/sink/onsuccess/OnSuccess.java
+++ examples/src/main/java/io/numaproj/numaflow/examples/sink/onsuccess/OnSuccess.java
@@ -15,7 +15,7 @@ import java.util.Random;
 @Slf4j
 public class OnSuccess extends Sinker {
     public static void main(String[] args) throws Exception {
-        Server server = new Server(new SimpleSink());
+        Server server = new Server(new OnSuccess());
 
         // Start the server
         server.start();
@@ -41,13 +41,20 @@ public class OnSuccess extends Sinker {
             }
             try {
                 String msg = new String(datum.getValue());
-                log.info("Received message: {}, headers - {}", msg, datum.getHeaders());
+                log.info("Received message: {}, id: {}, headers - {}", msg, datum.getId(), datum.getHeaders());
                 if (writeToPrimarySink()) {
-                    responseListBuilder.addResponse(Response.responseOnSuccess(datum.getId(), (OnSuccessMessage) null));
+                    log.info("Writing to onSuccess sink: {}", datum.getId());
+                    responseListBuilder.addResponse(Response.responseOnSuccess(datum.getId(),
+                            OnSuccessMessage.builder()
+                                    .value(String.format("Successfully wrote message with ID: %s",
+                                            datum.getId()).getBytes())
+                                    .build()));
                 } else {
+                    log.info("Writing to fallback sink: {}", datum.getId());
                     responseListBuilder.addResponse(Response.responseFallback(datum.getId()));
                 }
             } catch (Exception e) {
+                log.warn("Error while writing to any sink: ", e);
                 responseListBuilder.addResponse(Response.responseFailure(
                         datum.getId(),
                         e.getMessage()));
@@ -59,7 +66,7 @@ public class OnSuccess extends Sinker {
     /**
      * Example method to simulate write failures/success to primary sink.
      * Based on whether this returns true/false, we write to fallback sink / onSuccess sink
-     * @return
+     * @return true if simulated write to primary sink is successful, false otherwise
      */
     public boolean writeToPrimarySink() {
         Random random = new Random();
